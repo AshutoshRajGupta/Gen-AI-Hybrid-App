a367aea0d31dd3ffa10d60e6a144646fe19cd824
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2025-08-30 15:49:33 +0530

    Update blog_generator.py

--- blog_generator.py
+++ blog_generator.py
@@ -5,8 +5,9 @@ load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from modules.prompts import BLOG_PROMPT
-
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+import streamlit as st 
+google_api_key = st.secrets["GOOGLE_API_KEY"]
+os.environ["GOOGLE_API_KEY"] = google_api_key
 
 def generate_blog(topic: str, tone: str = "formal", prompt_template: str = BLOG_PROMPT) -> str:
     """Generate a blog given a topic, tone, and prompt template using Gemini."""
@@ -14,3 +15,4 @@ def generate_blog(topic: str, tone: str = "formal", prompt_template: str = BLOG_
     prompt = prompt_template.format(topic=topic, tone=tone)
     response = llm.invoke(prompt)
     return response.content
+
